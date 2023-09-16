de9dd9ff709ff354d4444dd6081b35646ed2ec98
Cheryl <106958690+ccp84@users.noreply.github.com>
2023-09-16 19:14:17 +0100

    form values as a number

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,21 +1,45 @@
+import { useState, useEffect } from "react";
+
+
 const Counter = () => {
+
+  const [count, setCount] = useState<number>(0)
+  const [holdCount, setholdCount] = useState<number>(count)
+
+  useEffect(() => {
+    setholdCount(count)
+  }, [count])
+  
+
   return (
     <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4 shadow-lg border-primary-500">
       <h1>Days Since the Last Accident</h1>
-      <p className="text-6xl">0</p>
+      <p className="text-6xl">{count}</p>
       <div className="flex gap-2">
-        <button>➖ Decrement</button>
-        <button>🔁 Reset</button>
-        <button>➕ Increment</button>
+        <button  
+        onClick={() => setCount((count) => count - 1)}
+        >➖ Decrement</button>
+        <button
+        onClick={() => setCount(0)}
+        >🔁 Reset</button>
+        <button
+        onClick={() => setCount((count) => count + 1)}
+        >➕ Increment</button>
       </div>
       <div>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input type="number" value={0} />
-          <button type="submit">Update Counter</button>
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          setCount(holdCount) } }>
+          <input type="number" value={holdCount}
+          onChange={(e) => setholdCount(e.target.valueAsNumber)} />
+          <button 
+          type="submit"
+          
+          >Update Counter</button>
         </form>
       </div>
     </section>
   );
 };
 
-export default Counter;
+export default Counter
